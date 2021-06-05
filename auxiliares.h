eacbf24a122a7f1ed751078b0de069c06701aca3
f7olivera <f7olivera@gmail.com>
2021-06-04 23:41:25 -0300

    Ejercicio 1 terminado.

--- auxiliares.h
+++ auxiliares.h
@@ -20,4 +20,14 @@ tablero tableroActual ( posicion const &p );
 void tableroLindo(const posicion& p);
 void tableroFeo(string tableroFEN);
 
+int aparicionesEnTablero(tablero t, casilla p);
+
+bool esTableroValido(const tablero& t);
+bool esMatriz(tablero t);
+bool casillasValidas(tablero t);
+bool piezaValida(casilla c);
+bool sinPeonesNoCoronados(tablero t);
+bool cantidadValidaDePiezas(const tablero& t);
+int aparicionesEnTablero(tablero t, casilla p);
+
 #endif //AJEDREZLITE_AUXILIARES_H
