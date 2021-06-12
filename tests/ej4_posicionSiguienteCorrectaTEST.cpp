a57a8b60761578f29375990e755a651fb516c5ea
f7olivera <f7olivera@gmail.com>
2021-06-12 15:41:00 -0300

    Agrego test.
    Para cubrir la conversión de peón a torre en piezaCorrectaEnDestino.

--- tests/ej4_posicionSiguienteCorrectaTEST.cpp
+++ tests/ej4_posicionSiguienteCorrectaTEST.cpp
@@ -71,3 +71,35 @@ TEST(posicionSiguienteCorrectaTEST, peonHaceSaltoDelTigre)
 
     EXPECT_FALSE(posicionSiguienteCorrecta(p, q, o, d));
 }
+
+TEST(posicionSiguienteCorrectaTEST, peonCoronado)
+{
+    tablero t_p = {
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cREY_N,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cPEON_N,cVACIA,cVACIA,cVACIA,cVACIA,cREY_B},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+    };
+    tablero t_q = {
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cREY_N,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA,cREY_B},
+            {cVACIA,cVACIA,cTORRE_N,cVACIA,cVACIA,cVACIA,cVACIA,cVACIA},
+    };
+
+    posicion p = make_pair(t_p, NEGRO);
+    posicion q = make_pair(t_q, BLANCO);
+
+    coordenada o = setCoord(6, 2);
+    coordenada d = setCoord(7, 2);
+
+    EXPECT_TRUE(posicionSiguienteCorrecta(p, q, o, d));
+}
